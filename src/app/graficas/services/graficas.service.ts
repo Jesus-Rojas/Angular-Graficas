5e1c19f3be85885680b6155896ebe818c46d65ed
Jesus-Rojas <jarojas6524@misena.edu.co>
2021-07-13 22:44:41 -0500

    listo para produccion

--- src/app/graficas/services/graficas.service.ts
+++ src/app/graficas/services/graficas.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map, delay } from 'rxjs/operators'
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GraficasService {
 
+  ruta=environment.ruta;
+
   constructor( private http: HttpClient ) { }
 
   getUsuariosRedesSociales() {
-    return this.http.get('http://localhost:3000/grafica');
+    return this.http.get(`${this.ruta}/grafica`);
   }
 
   getUsuariosRedesSocialesDonaData() {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  ruta: `https://json-server-app-graficas.herokuapp.com`
 };
